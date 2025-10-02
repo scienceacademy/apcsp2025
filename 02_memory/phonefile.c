9a4d53505e072bc48b9482459aaaf78b27a9b101
Chris Bradfield <cb@scribe.net>
2025-10-02 16:56:45 -0700

    updates

---
+++ 02_memory/phonefile.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+   FILE *file = fopen("phonebook.csv", "a");
+   if (!file)
+   {
+        return 1;
+   }
+
+   char *name = get_string("name: ");
+   char *number = get_string("number: ");
+
+   fprintf(file, "%s,%s\n", name, number);
+   fclose(file);
+}
